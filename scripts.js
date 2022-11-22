411a784f9fc5a19ca1e8ef9f6220054d6f0cfc02
Sunil Pavar <55226366+sunilpavar98@users.noreply.github.com>
2022-11-22 17:35:24 +0530

    Wather App Using HTML CSS And JS

---
+++ scripts.js
@@ -0,0 +1,34 @@
+getLocation();
+
+function getLocation(){
+    
+}
+
+function showPotion(position){
+    
+}
+
+async function getName(lat,longi){
+    var api_key = 'fc3a65fc9ebd7761b6cd0379e6cb3775';
+    var api_url = 'https://api.openweathermap.org/geo/1.0/reverse?lat=' + lat + '&lon=' + longi + '&limit=5&appid=' + api_key;''
+
+    
+
+}
+
+async  function fetchData(location){
+    var api_url = 'https://api.weatherapi.com/v1/current.json?key=1c63857a8e0548f9a1a152750210909&q=' + location + '&aqi=yes';
+
+    
+    
+    
+}
+
+function setValue(data){
+    
+    
+}
+
+function findWeather(){
+   
+}
